dff7d620a94c66fb21316f8850fde4fa7a60b1ae
Kevin Wu <kevinwu.business@gmail.com>
2015-03-01 12:10:16 -0800

    Added LESS and fixed some issues.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressLess = require('express-less');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var classifier = require('./routes/classifier');
 
 var app = express();
@@ -21,10 +21,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressLess(path.join(__dirname, 'public', 'stylesheets')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/classifier', classifier);
 
 // catch 404 and forward to error handler
