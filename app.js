460196f83918f5b5881fb8f155bb22acdc57301f
Kevin Wu <kevinwu.business@gmail.com>
2015-03-01 13:55:03 -0800

    Implemented basic landing page.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(expressLess(path.join(__dirname, 'public', 'stylesheets')));
+app.use(expressLess(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
