81d4b267e405ae459d07a4b0c0dd5391f9c9422e
Wenson Hsieh <whsieh@berkeley.edu>
2015-03-01 18:01:57 -0800

    Enter now navigates user from index to analysis page.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,14 +4,20 @@ $(function() {
     var $regionGrp = $(".region");
     var $roleGrp = $(".role");
 
-    $searchBtn.click(function() {
+    var navigateToAnalysis = function() {
         var region = $regionGrp.find("button .value").text().toLowerCase();
         var role = $roleGrp.find("button .value").text().toLowerCase();
         var username = $usernameInput[0].value.toLowerCase();
         var url = "/analysis";
 
         window.location = url + "?" + $.param({role: role, region: region, username: username});
-    });
+    }
+    $searchBtn.click(navigateToAnalysis);
+     $(window).bind("keypress", function(e) {
+        if (e.keyCode == 13) {
+            navigateToAnalysis();
+        }
+     });
 
     $regionGrp.add($roleGrp).find("li a").click(function() {
         $this = $(this)
